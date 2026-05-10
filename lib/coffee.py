dcfd1cb72d413d9eb272431fcd2eaf53d06fc61b
Allison Valdez <allison.snipe@gmail.com>
2026-05-10 10:23:08 -0700

    done

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,30 +1,30 @@
 # Class for Coffee
-
 class Coffee:
-
     # Constructor for Coffee class
     def __init__(self, size, price):
-        self.name = name
-        self.caffeine_content = caffeine_content
+        self.size = size      
+        self.price = price    
     
-    # Method to get the size of the coffee
+    # Getter for size with decorator
+    @property                 
     def size(self):
         # Getter it returns the size of the coffee
-        return self.size
+        return self._size     
 
-    # Method to get the price and size of the coffee
+    # Setter for size with error handling
+    @size.setter              
     def size(self, value):
         # Check if value is one of the valid sizes
         if value not in ["Small", "Medium", "Large"]:
             # Print an error if not
-            print("Error: size must be 'Small', 'Medium', or 'Large'.")
+            print("Size must be Small, Medium, or Large.") 
         else:
             # Stores the value if it is valid
             self._size = value
     
-    # Method to get the tip amount
+    # Method to tip for the coffee
     def tip(self):
         # Print the message
         print("This coffee is great, here's a tip!")
-        # Increase the tip by $1.00
-        self.price += 1.00
+        # Increase the price by $1.00
+        self.price += 1
